d4cff3b42198ef08d6a18778842215e703cce243
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-16 21:09:13 -0800

    made color-in function triggered by hover effect

--- script.js
+++ script.js
@@ -8,11 +8,25 @@ function createDivs(dimensions) {
   for (var i = 0; i < dimensions * dimensions; i++) {
     let div = document.createElement("div");
     div.classList.add("grid-box");
-    div.textContent = "hi";
-    div.style.width = "20px";
-    div.style.height = "20px";
+    div.style.width = "100%";
+    div.style.height = "0";
+    div.style.paddingBottom = "100%";
     gridContainer.appendChild(div);
   }
 }
 
+// function colorIn(el) {
+//   let el = el;
+// }
+
 createDivs(16);
+
+const divs = document.querySelectorAll(".grid-box");
+// div.addEventListener("mouseover", () => {
+//   console.log("mouse, over,.");
+// });
+divs.forEach((el) =>
+  el.addEventListener("mouseover", () => {
+    el.style.backgroundColor = "red";
+  })
+);
