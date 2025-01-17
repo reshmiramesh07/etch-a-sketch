94afcd4a314c6bf90b8fe65ce58f1c96fcbf68fd
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-16 20:41:42 -0800

    formatted divs into grid

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const container = document.querySelector(".grid-container");
+/** @type {HTMLElement | null} */
+const gridContainer = document.querySelector("#grid-container");
 
 function createDivs(dimensions) {
+  gridContainer.style.display = "grid";
+  gridContainer.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`;
   for (var i = 0; i < dimensions * dimensions; i++) {
     let div = document.createElement("div");
     div.classList.add("grid-box");
-    // div.style.borderWidth = "thick";
-    // div.style.borderColor = "black";
-    container.appendChild(div);
+    div.textContent = "hi";
+    div.style.width = "20px";
+    div.style.height = "20px";
+    gridContainer.appendChild(div);
   }
 }
 
-createDivs(4);
+createDivs(16);
